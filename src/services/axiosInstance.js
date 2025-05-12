ceeb4773b415ef83d8690c9092c715aedfcc870b
sayan2302 <sayan.pro.id@gmail.com>
2025-05-12 06:13:47 +0530

    Refactor code structure for improved readability and maintainability

--- src/services/axiosInstance.js
+++ src/services/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:8000',
+    // baseURL: 'http://localhost:8000',
+    baseURL: 'https://vaccination-portal-backend.vercel.app',
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json',
